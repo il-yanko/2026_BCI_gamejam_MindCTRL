8e29185ed6e366fb5629f575c1d090e9aa6c969a
scribblemaniac <scribblemaniac@users.noreply.github.com>
2026-03-03 16:19:46 -0700

    Add audio mixer

--- SandboxProject/Assets/Scenes/BarAdjust.cs
+++ SandboxProject/Assets/Scenes/BarAdjust.cs
@@ -9,7 +9,7 @@ public class BarAdjust : MonoBehaviour
     public GameObject face;
     public Sprite faceExpression;
     public float height;
-    
+
     private AudioSource audioSource;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,13 +20,13 @@ public class BarAdjust : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    
+
     public void Trigger() {
         StartCoroutine(DoStuff());
     }
-    
+
     private IEnumerator DoStuff() {
         Vector3 position = bar.transform.position;
         Vector3 scale = bar.transform.localScale;
@@ -34,12 +34,12 @@ public class BarAdjust : MonoBehaviour
         float newY = bottomY + height / 2;
         face.transform.position = bar.transform.position = new Vector3(position.x, newY, position.z);
         bar.transform.localScale = new Vector3(scale.x, height, scale.y);
-        
+
         face.GetComponent<Image>().overrideSprite = faceExpression;
-        
+
         audioSource.Play();
         yield return new WaitUntil(() => audioSource.isPlaying == false);
-        
+
         face.GetComponent<Image>().overrideSprite = null;
     }
 }
